6283f4f3e35e598ceaea6c2e5a3e01782e20d241
Muqiu Han <muqiu-han@outlook.com>
2023-03-07 17:48:55 +0800

    Fix the problem that SayHello module sends JSON OBJECT

--- AutumnBot.Service/AutumnBot.Service.SayHello/index.js
+++ AutumnBot.Service/AutumnBot.Service.SayHello/index.js
@@ -7,7 +7,7 @@ ws.on("error", console.error);
 ws.on("open", function open() {
   console.log("Successfully connected to AutumnBot.Core");
 
-  ws.send(JSON.toString({
+  ws.send(JSON.stringify({
     header: "AutumnBot.Service.SayHello",
     client: "",
     body: "",
@@ -17,7 +17,7 @@ ws.on("open", function open() {
 ws.on("message", function message(data) {
   let header = JSON.parse(data)["header"];
 
-  ws.send(JSON.toString({
+  ws.send(JSON.stringify({
     header: "AutumnBot.Service.SayHello",
     client: header,
     body: "Hello!",
