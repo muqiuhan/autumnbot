d41356872b1ff70d083c50d82876702a7b7d12c0
Muqiu Han <muqiu-han@outlook.com>
2023-03-07 21:03:38 +0800

    Fixed Ocason__Util.TypeError in the Dispatch module of AutumnBot.Core

--- AutumnBot.Core/lib/dispatch.ml
+++ AutumnBot.Core/lib/dispatch.ml
@@ -2,24 +2,50 @@ open Base
 open Ws_ocaml
 module Domain = Stdlib.Domain
 
-let dispatch () : unit =
-  while true do
-    match Message.get () with
-    | Message.Client (header, { service; service_body }) ->
-      Log.debug ("Dispatch process a client message from : " ^ header);
-      Option.iter (Instance.find_client header) ~f:(fun client ->
-        match Instance.find_service service with
-        | None -> Exception.raise client (Exception.Service_not_found_exn service)
-        | Some service ->
-          Websocket.send_text service (Bytes.of_string service_body) |> ignore)
-    | Message.Service (header, { client; client_body }) ->
-      Log.debug ("Dispatch process a service message from : " ^ header);
-      Option.iter (Instance.find_service header) ~f:(fun service ->
-        match Instance.find_client client with
-        | None -> Exception.raise service (Exception.Client_not_found_exn client)
-        | Some client ->
-          Websocket.send_text client (Bytes.of_string client_body) |> ignore)
-  done
+let check_send_status = function
+  | true -> Log.info "Processed successfully"
+  | false -> Log.error "Processing failed"
+;;
+
+let rec dispatch () : unit =
+  try
+    while true do
+      match Message.get () with
+      | Message.Client (header, { service; service_body }) ->
+        Option.iter (Instance.find_client header) ~f:(fun client ->
+          Log.debug ("Dispatch process a client message from " ^ header ^ " to " ^ service);
+          match Instance.find_service service with
+          | None -> Exception.raise client (Exception.Service_not_found_exn service)
+          | Some service ->
+            Websocket.send_text
+              service
+              (Ocason.Basic.JsonObject
+                 [ "header", Ocason.Basic.JsonString header
+                 ; "body", Ocason.Basic.JsonString service_body
+                 ]
+              |> Ocason.Basic.to_string
+              |> Bytes.of_string)
+            |> check_send_status)
+      | Message.Service (header, { client; client_body }) ->
+        Option.iter (Instance.find_service header) ~f:(fun service ->
+          Log.debug ("Dispatch process a service message from " ^ header ^ " to " ^ client);
+          match Instance.find_client client with
+          | None -> Exception.raise service (Exception.Client_not_found_exn client)
+          | Some client ->
+            Websocket.send_text
+              client
+              (Ocason.Basic.JsonObject
+                 [ "header", Ocason.Basic.JsonString header
+                 ; "body", Ocason.Basic.JsonString client_body
+                 ]
+              |> Ocason.Basic.to_string
+              |> Bytes.of_string)
+            |> check_send_status)
+    done
+  with
+  | e ->
+    Log.error (Stdlib.Printexc.to_string e);
+    dispatch ()
 ;;
 
 let start () =
