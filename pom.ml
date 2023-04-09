e2a7a81ad2ee2a68f04eb641459dde56be7ae2e5
Muqiu Han <muqiu-han@outlook.com>
2023-04-09 22:45:23 +0800

    👌 IMPROVE: Make pom.ml easier to use

--- pom.ml
+++ pom.ml
@@ -1,37 +1,33 @@
 #!/bin/env ocaml
 
-module Module = struct
-  type config = 
-    { path: string;
-      build: string;
-      dev: string;
-      run: string;
-      setup: string list }
-end
+type config = 
+  { path: string;
+    build: string;
+    dev: string;
+    run: string;
+    setup: string list }
 
-module type Module_Sig = sig
-  val config : Module.config
-end
+module type Module = sig val config : config end
 
 let modules = [
   ("Core", 
     (module struct let config = 
-      Module.
         { path = "./Core";
           build = "dune build --release";
           dev = "dune build -w";
           run = "dune exec Core";
-          setup = ["opam install . --deps-only"] } end : Module_Sig))
+          setup = ["opam install . --deps-only"] } end : Module))
 ]
 
-let get_action (module M : Module_Sig) (action : string) = 
-  (match action with
-  | "build" -> M.config.build
-  | "setup" -> M.config.setup |> String.concat "&&"
-  | "dev" -> M.config.dev
-  | "run" -> M.config.run
-  | _ -> failwith "Unsupported action")
-  |> Format.sprintf "cd %s && %s" M.config.path
+let get_action (module M : Module) (action : string) = 
+  begin
+    match action with
+    | "build" -> M.config.build
+    | "setup" -> M.config.setup |> String.concat "&&"
+    | "dev" -> M.config.dev
+    | "run" -> M.config.run
+    | _ -> failwith "Unsupported action"
+  end |> Format.sprintf "cd %s && %s" M.config.path
 
 let _ = 
   let target = Sys.argv.(1)
