2242dc7e682e44b065a25a9fbd6898bf03cc9432
Muqiu Han <muqiu-han@outlook.com>
2024-06-24 23:34:54 +0800

    [autumnbot]: update project info

--- autumnbot/autumnbot/main.py
+++ autumnbot/autumnbot/main.py
@@ -1,3 +1,31 @@
+# Copyright (c) 2024 Muqiu Han
+# 
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright notice,
+#       this list of conditions and the following disclaimer in the documentation
+#       and/or other materials provided with the distribution.
+#     * Neither the name of AutumnBot nor the names of its contributors
+#       may be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import cv2
 import typing
 import pykka
@@ -6,10 +34,12 @@ from services.camera_saver.camera_saver import CameraSaver
 from services.service import Service
 from services.service_manager import ServiceManager
 
+# These are all services of AutumnBot
 SERVICES: set[typing.Type[Service]] = {CameraSaver}
 
 
-def main() -> None:
+# An example of managing and using all services through ServiceManager
+def example() -> None:
     service_manager = ServiceManager(SERVICES)
     service_manager.start_all_service()
 
@@ -23,3 +53,7 @@ def main() -> None:
                 cv2.waitKey(0)
     finally:
         service_manager.stop_all_service()
+
+
+def main() -> None:
+    example()
--- autumnbot/services/camera_saver/camera_saver.py
+++ autumnbot/services/camera_saver/camera_saver.py
@@ -1,23 +1,51 @@
-# pyright: reportReturnType=false
+# Copyright (c) 2024 Muqiu Han
+# 
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright notice,
+#       this list of conditions and the following disclaimer in the documentation
+#       and/or other materials provided with the distribution.
+#     * Neither the name of AutumnBot nor the names of its contributors
+#       may be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import typing
 import cv2
 from .. import service
 
 
+# Capture a frame from the camera
 class CameraSaver(service.Service):
     class_name = "CameraSaver"
     camera0: cv2.VideoCapture = cv2.VideoCapture(0)
 
     def __init__(self) -> None:
         self.info("initialize")
-        super().__init__("CameraSaver")
+        super().__init__()
 
     def on_start(self) -> None:
         self.info("start")
         return super().on_start()
 
-    def on_receive(self, message: typing.Any) -> typing.Optional[cv2.Mat]:
+    # Returns a frame of the camera, or None if an error occurs
+    def on_receive(self, message: typing.Any) -> typing.Optional[cv2.typing.MatLike]:
         self.info("Request to obtain the current camera picture")
         ret, frame = self.camera0.read()
 
